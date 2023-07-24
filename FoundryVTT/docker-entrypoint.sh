ce1ab5725f4c8e8881c0c57b23e19ed809749b09
Luis Roberto Lozano de los Santos <luisrlozano@gmail.com>
2023-07-24 07:36:48 -0600

    nginx best practices

--- FoundryVTT/docker-entrypoint.sh
+++ FoundryVTT/docker-entrypoint.sh
@@ -1,16 +1,23 @@
 #!/usr/bin/env ash
-echo "$VARS" | base64 -d | xargs -d'|' -i export {}
+decoded_str=$(echo "$base64_vars" | tr '|' ' ')
+
+for var in $decoded_str; do
+    export "$var"
+done
+
+echo "FQDN=$FQDN"
+echo "PROXY_PORT=$PROXY_PORT"
+
 if [ -f "/home/foundry/.firstboot" ]; then
-    echo "Firstboot!"
+    echo " - Firstboot!"
     rm -rf /home/foundry/.firstboot
 else
     sed -i 's/"hostname": null,/"hostname": "${FQDN}",/' /home/foundry/userdata/Config/options.json
-    sed -i 's/"proxyPort": null,/"proxyPort": "${PROXY_PORT}",/' /home/foundry/userdata/Config/options.json
+    sed -i 's/"proxyPort": null,/"proxyPort": ${PROXY_PORT},/' /home/foundry/userdata/Config/options.json
 fi
 
 cd /home/foundry/vtt/
 export USER=$(whoami)
-echo "Running FoundryVTT as $USER"
-echo " "
-echo "Running pm2 app VTT ..."
+echo " - Running FoundryVTT as $USER"
+echo " - Running pm2 app foundry ..."
 pm2-runtime start --env production --name foundry resources/app/main.js -- --dataPath=/home/foundry/userdata
